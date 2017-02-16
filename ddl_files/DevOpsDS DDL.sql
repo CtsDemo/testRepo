964dcf1925770494c618e04f0816b86bc744f63c
467095 <467095@100.96.136.143>
2017-02-16 16:59:53 +0530

    Committed by baalaji1983

--- ddl_files/DevOpsDS DDL.sql
+++ ddl_files/DevOpsDS DDL.sql
@@ -1,6 +1,6 @@
 /*
 Data Store: DevOpsDS
-Created: 2017-02-16T10:41:56.076Z
+Created: 2017-02-16T10:46:50.898Z
 By: 467095
 Appian Version: 17.1.0.0
 Target Database: MySQL 5.6.11
@@ -8,13 +8,6 @@ Database Driver: MySQL Connector Java mysql-connector-java-5.1.31 ( Revision: al
 */
 
 /* UPDATE DDL */
-    create table `devopscdt` (
-        `id` integer not null auto_increment,
-        `firstname` varchar(255),
-        `lastname` varchar(255),
-        primary key (`id`)
-    ) ENGINE=InnoDB;
-
 /* DROP AND CREATE DDL */
 /* WARNING: The DDL commented out below will drop and re-create all tables.
     drop table if exists `devopscdt`;
